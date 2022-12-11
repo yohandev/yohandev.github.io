b1dde15678a7cb35ef3cebe1fefec7559917973e
Yohan Guyomard <yohang@mit.edu>
2022-12-10 23:15:02 -0500

    Added buttons under video player

--- src/views/watch.js
+++ src/views/watch.js
@@ -2,6 +2,7 @@ import { FilterBar } from '../components/filters';
 import { NavigationBar } from '../components/topbar';
 import { VideoRecommendations } from '../components/video';
 import './watch.css';
+import '../components/button.css';
 
 import xwing from "../../www/assets/videos/xwing.json";
 import { useEffect, useState } from 'preact/hooks';
@@ -37,6 +38,30 @@ export const Watch = ({id}) => {
                         <div id="subscribers">
                             139k subscribers
                         </div>
+                    </div>
+                    <button id="subscribe-button" class="touch-button">
+                        Subscribed
+                    </button>
+                    <div id="skills">
+                        <button class="touch-button">
+                            C#
+                            <div id="tooltip">
+                                This project uses C#
+                            </div>
+                        </button>
+                        <button class="touch-button">
+                            Unity
+                            <div id="tooltip">
+                                This project is made in Unity
+                            </div>
+                        </button>
+                        <button class="touch-button">
+                            <i class="fa-solid fa-share"></i>
+                            &nbsp;Share
+                            <div id="tooltip">
+                                Share
+                            </div>
+                        </button>
                     </div>
                 </div>
             </div>
